cfd432ca7e44eb468b1f9fc093ae28b9dab886ec
Alvin Syahbana <dev.alvin.syahbana@gmail.com>
2022-10-26 23:39:53 +0700

    update schema

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -16,12 +16,12 @@ import (
 // CreateRfq is the resolver for the createRFQ field.
 func (r *mutationResolver) CreateRfq(ctx context.Context, input model.NewRfq) (*model.Rfq, error) {
 	sqlStatement := `INSERT INTO rfq.header(
-		"CompanyName", "CompanyAddress", "CompanyWebsite", "QuotationDate", "QuotationNo", "QuotationExpires", "MadeForName", "MadeForAddress", "MadeForPhone", "SentToName", "SentToAddress", "SentToPhone", "Disc", "Tax", "Interest", "SNK")
-		VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *;`
+		"CompanyName", "CompanyAddress", "CompanyWebsite", "QuotationDate", "QuotationNo", "QuotationExpires", "MadeForName", "MadeForAddress", "MadeForPhone", "SentToName", "SentToAddress", "SentToPhone", "Disc", "Tax", "Interest", "SNK", "Status")
+		VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *;`
 	response := &model.Rfq{}
 	items := []*model.Item{}
 
-	err := r.DB.QueryRow(sqlStatement, input.CompanyName, input.CompanyAddress, input.CompanyWebsite, input.QuotationDate, input.QuotationNo, input.QuotationExpires, input.MadeForName, input.MadeForAddress, input.MadeForPhone, input.SentToName, input.SentToAddress, input.SentToPhone, input.Disc, input.Tax, input.Interest, pq.Array(input.Snk)).Scan(
+	err := r.DB.QueryRow(sqlStatement, input.CompanyName, input.CompanyAddress, input.CompanyWebsite, input.QuotationDate, input.QuotationNo, input.QuotationExpires, input.MadeForName, input.MadeForAddress, input.MadeForPhone, input.SentToName, input.SentToAddress, input.SentToPhone, input.Disc, input.Tax, input.Interest, pq.Array(input.Snk), input.Status).Scan(
 		&response.CompanyName,
 		&response.CompanyAddress,
 		&response.CompanyWebsite,
@@ -38,15 +38,39 @@ func (r *mutationResolver) CreateRfq(ctx context.Context, input model.NewRfq) (*
 		&response.Tax,
 		&response.Interest,
 		pq.Array(&response.Snk),
-		&response.ID)
+		&response.ID,
+		&response.CreatedAt,
+		&response.CreatedBy,
+		&response.UpdatedAt,
+		&response.UpdatedBy,
+		&response.Status,
+		&response.DeletedAt,
+		&response.DeletedBy,
+		&response.IsDeleted)
 	for _, item := range input.Items {
 		newItem := &model.Item{}
+		// debuging
+		// log.Print(`INSERT INTO rfq.items(
+		// 	"HeaderID", "Nama", "Harga", "Qty")
+		// 	VALUES ($1, $2, $3, $4) RETURNING *;`, response.ID, item.Nama, item.Harga, item.Qty)
 
 		println(response.ID)
 		sqlStatementItem := `INSERT INTO rfq.items(
 			"HeaderID", "Nama", "Harga", "Qty")
 			VALUES ($1, $2, $3, $4) RETURNING *;`
-		err := r.DB.QueryRow(sqlStatementItem, response.ID, item.Nama, item.Harga, item.Qty).Scan(&newItem.HeaderID, &newItem.Nama, &newItem.Harga, &newItem.Qty)
+		err := r.DB.QueryRow(sqlStatementItem, response.ID, item.Nama, item.Harga, item.Qty).Scan(
+			&newItem.HeaderID,
+			&newItem.Nama,
+			&newItem.Harga,
+			&newItem.Qty,
+			&newItem.CreatedAt,
+			&newItem.CreatedBy,
+			&newItem.UpdatedAt,
+			&newItem.UpdatedBy,
+			&newItem.DeletedAt,
+			&newItem.DeletedBy,
+			&newItem.IsDeleted,
+		)
 		if err != nil {
 			panic(err)
 		}
@@ -65,7 +89,7 @@ func (r *mutationResolver) Login(ctx context.Context, input model.Login) (*model
 	err := r.DB.QueryRow("SELECT * FROM rfq.user WHERE email = ($1) AND password = ($2)", input.Email, input.Password).Scan(&user.Email, &user.Password)
 
 	if err != nil {
-		return nil, fmt.Errorf("Invalid login")
+		return nil, fmt.Errorf("invalid login")
 	}
 	token, _ := service.JwtGenerate(user.Email)
 	response := model.LoginResponse{Token: token}
@@ -94,10 +118,47 @@ func (r *mutationResolver) Rfq(ctx context.Context, input model.RFQInput) (*mode
 		&response.Interest,
 		pq.Array(&response.Snk),
 		&response.ID,
+		&response.CreatedAt,
+		&response.CreatedBy,
+		&response.UpdatedAt,
+		&response.UpdatedBy,
+		&response.Status,
+		&response.DeletedAt,
+		&response.DeletedBy,
+		&response.IsDeleted,
 	)
+
 	if err != nil {
 		panic(err)
 	}
+
+	rowsItems, errItems := r.DB.Query(`SELECT * FROM rfq.items WHERE "HeaderID" = ($1)`, input.ID)
+
+	if errItems != nil {
+		panic(errItems)
+	}
+
+	responseArray := []*model.Item{}
+	for rowsItems.Next() {
+		responseItem := &model.Item{}
+		rowsItems.Scan(
+			&responseItem.HeaderID,
+			&responseItem.Nama,
+			&responseItem.Harga,
+			&responseItem.Qty,
+			&responseItem.CreatedAt,
+			&responseItem.CreatedBy,
+			&responseItem.UpdatedAt,
+			&responseItem.UpdatedBy,
+			&responseItem.DeletedAt,
+			&responseItem.DeletedBy,
+			&responseItem.IsDeleted,
+		)
+		responseArray = append(responseArray, responseItem)
+	}
+
+	response.Items = responseArray
+
 	return response, nil
 }
 
